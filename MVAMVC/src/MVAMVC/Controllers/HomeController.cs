c72cf4f4de40ef4f921d3d60d5c56d1fd0bf114c
pallen7 <paulallen7@msn.com>
2016-03-16 18:28:52 +0000

    Following tutorial

---
+++ MVAMVC/src/MVAMVC/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+using MVAMVC.Models;
+
+namespace MVAMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // GET /HOME/ABOUT
+        [HttpGet]
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Here is a person.";
+
+            var person = new Person()
+            {
+                first_name = "Paul",
+                surname = "Allen",
+                birthdate = new DateTime(1990,2,25)
+            };
+
+            return View(person);
+        }
+
+        [HttpPost]
+        public IActionResult About(Person person)
+        {
+            return View(person);
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVAMVC/src/MVAMVC/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MVAMVC.Models
+{
+    public class Person
+    {
+        public string first_name { get; set; }
+        public string surname { get; set; }
+        public DateTime birthdate { get; set; }
+    }
+}
